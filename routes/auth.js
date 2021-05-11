1325c3569ab8c44f13e616cabedcef286a89de84
yosrimhamdi <bavary1515@gmail.com>
2021-05-11 17:03:45 +0100

    using post on logut.

--- routes/auth.js
+++ routes/auth.js
@@ -9,7 +9,7 @@ const router = Router();
 
 router.use('/password', passwordRouter);
 
-router.get('/logout', logout);
+router.post('/logout', logout);
 router.post('/signup', signup);
 router.post('/login', login);
 
