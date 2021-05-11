07708bdf2bac11c6f736904be3b4d416bc64c987
yosrimhamdi <bavary1515@gmail.com>
2021-05-11 14:22:44 +0100

    using the catcher.

--- controllers/auth/signup.js
+++ controllers/auth/signup.js
@@ -3,14 +3,10 @@ import User from '../../models/User';
 export default async (req, res, next) => {
   const { email, password } = req.body;
 
-  try {
-    const user = await User.create({ email, password });
+  const user = await User.create({ email, password });
 
-    req.user = user;
-    req.status = 201;
+  req.user = user;
+  req.status = 201;
 
-    next();
-  } catch (e) {
-    next(e);
-  }
+  next();
 };
