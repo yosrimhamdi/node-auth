b43a3ca8b4f310e9ae90cd9436849b4bab23158a
yosrimhamdi <bavary1515@gmail.com>
2021-05-11 17:01:43 +0100

    [fix] mongoose node warnings.

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ import app from './app';
 mongoose.connect('mongodb://localhost/test', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: true,
 });
 
 app.listen(3000);
